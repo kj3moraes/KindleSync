573b2988d086403af9bc00c4cf659c7cde6f4787
Keane Moraes <keanejonathan3@gmail.com>
2023-03-01 17:11:11 -0500

    added a simple API with / read on files

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
 
 // Create a server
 const server = http.createServer((req, res) => {
@@ -10,6 +11,22 @@ const server = http.createServer((req, res) => {
         res.end("This is the HOME page!");
     } else if (pathName == "/overview") {
         res.end("This is the CURRENT OVERVIEW page!")
+    } else if (pathName == "/api") {
+        fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
+
+            if (err != null) {
+                console.log(err);
+                res.writeHead(404, {
+                    'Content-type': 'text/html',
+                });
+            } 
+            const productData = JSON.parse(data); // Convert the data to a JSON object
+            console.log(productData);
+            res.writeHead(200, {
+                'Content-type': 'application/json',
+            });
+            res.end(data);
+        });
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html',

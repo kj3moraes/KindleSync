bb3d2feb6a2bcf8592bbf7889c5e1498816a6acb
Keane Moraes <keanejonathan3@gmail.com>
2023-03-07 15:14:10 -0500

    change to the data and author display

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ const fs = require('fs');
 const replaceTemplate = (temp, product) => {
     let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
     output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%BY%}/g, product.by);
     output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
     output = output.replace(/{%PRICE%}/g, product.price);
     output = output.replace(/{%DESCRIPTION%}/g, product.description);
     output = output.replace(/{%ID%}/g, product.id);
@@ -33,7 +32,6 @@ const server = http.createServer((req, res) => {
 
     // Parse the URL
     const { query, pathname } = url.parse(req.url, true);
-    console.log(pathname)
 
     // Homepage
     if (pathname === "/" || pathname === "/overview") {
